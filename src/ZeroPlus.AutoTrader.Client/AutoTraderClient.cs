dd254539a3c7ee8d8367951918a9978e77575f19
chat-omega <chetan@omegaintelligence.ai>
2026-07-01 16:24:20 +0000

    Fix AutoTraderClient impl + add TransactionSubscriptionMode enum

--- src/ZeroPlus.AutoTrader.Client/AutoTraderClient.cs
+++ src/ZeroPlus.AutoTrader.Client/AutoTraderClient.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using ZeroPlus.AutoTrader.Client.Interfaces;
+using ZeroPlus.Models.Data.Models;
+using ZeroPlus.Models.Data.Trading;
 
 namespace ZeroPlus.AutoTrader.Client
 {
@@ -8,13 +11,24 @@ namespace ZeroPlus.AutoTrader.Client
     {
         public event Action? ClientConnected;
         public event Action? ClientDisconnected;
+        public event Action? AccountsAndRoutesInitialized;
         public bool IsConnected { get; private set; }
+        public List<Account> Accounts { get; private set; } = new()
+        {
+            new Account { AccountId = "DEMO001", Description = "Demo Account" }
+        };
 
         public async Task StartAsync()
         {
             await Task.CompletedTask;
             IsConnected = true;
-            _ = Task.Run(async () => { await Task.Delay(50); ClientConnected?.Invoke(); });
+            _ = Task.Run(async () =>
+            {
+                await Task.Delay(50);
+                ClientConnected?.Invoke();
+                await Task.Delay(50);
+                AccountsAndRoutesInitialized?.Invoke();
+            });
         }
 
         public async Task StopAsync()
@@ -22,6 +36,12 @@ namespace ZeroPlus.AutoTrader.Client
             await Task.CompletedTask;
             IsConnected = false;
             ClientDisconnected?.Invoke();
+        }
+
+        public async Task<List<Account>> RequestAccountsAsync()
+        {
+            await Task.CompletedTask;
+            return Accounts;
         }
     }
 }
---
+++ src/ZeroPlus.Comms.Models/TransactionSubscriptionMode.cs
@@ -0,0 +1,11 @@
+// Stub for internal enum - originally from a ZeroPlus internal package
+namespace ZeroPlus.Models.Data.Enums
+{
+    public enum TransactionSubscriptionMode
+    {
+        Off,
+        All,
+        Fills,
+        OwnAndFills
+    }
+}
